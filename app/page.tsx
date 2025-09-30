f823ca89c67f5e5f91bb5f410b05f87a7d5a3598
putra-pkwl03 <putrapongkowulu@gmail.com>
2025-09-30 17:07:53 +0700

    menambahkan component about ke page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import DestinationSection from "./components/DestinationSection";
+import AboutSection from "./components/AboutSection";
 import Footer from "./components/Footer";
 
 export default function HomePage() {
@@ -10,6 +11,7 @@ export default function HomePage() {
       <Navbar />
       <Hero />
       <DestinationSection />
+        <AboutSection />
       <Footer />
     </div>
   );
