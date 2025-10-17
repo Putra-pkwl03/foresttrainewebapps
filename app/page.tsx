11550080ac0f76e7dacb4f685f56f3d63885ca18
putra-pkwl03 <putrapongkowulu@gmail.com>
2025-10-17 08:38:36 +0700

    menambahkan artticle section ke page utama

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import DestinationSection from "./components/DestinationSection";
 import AboutSection from "./components/AboutSection";
+import ArticlesSection from "./components/ArticlesSection";
 import Footer from "./components/Footer";
 
 export default function HomePage() {
@@ -12,6 +13,7 @@ export default function HomePage() {
       <Hero />
       <DestinationSection />
         <AboutSection />
+        <ArticlesSection />
       <Footer />
     </div>
   );
